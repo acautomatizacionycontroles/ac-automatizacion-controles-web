a30cd76bf25b22ba095d5b6afb44cb7222a483d2
acautomatizacionycontroles <acautomatizacionycontroles@gmail.com>
2026-05-29 17:20:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// AC Automatización y Controles
 
+console.log("Sitio cargado correctamente");
+
+// Cambiar color del header al hacer scroll
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector("header");
+
+    if(window.scrollY > 50){
+        header.style.background = "rgba(5,7,10,.98)";
+    } else {
+        header.style.background = "rgba(11,15,20,.95)";
+    }
+
+});
